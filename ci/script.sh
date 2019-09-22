88a454b10b3e870332e2e014f95ee2c331793fc0
Hung-I Wang <whygowe@gmail.com>
2019-09-22 19:40:24 +0800

    Fix CI

--- ci/script.sh
+++ ci/script.sh
@@ -11,7 +11,7 @@ main() {
     cross test --target $TARGET
     # cross test --target $TARGET --release
 
-    cross run --target $TARGET
+    # cross run --target $TARGET
     # cross run --target $TARGET --release
 }
 
