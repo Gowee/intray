e4d4215986d498bf008b3b77bd4e0a52ff93c5cc
Hung-I Wang <whygowe@gmail.com>
2019-09-22 22:21:02 +0800

    Use strip and upx to compress binary for release

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,6 +1,17 @@
+#!/bin/bash
 # This script takes care of building your crate and packaging it for release
-
 set -ex
+
+try_compress() {
+    file=$1
+    target=$(objdump -a $file | grep -oP "(?<=format )[\w-]+" | tr -d '\n' || "")
+    if [ -n "$target" ] && strip -v $file --target $target; then
+        echo "Stripped $file (target: $target)."
+    fi
+    if upx $file; then
+        echo "Upx $file done."
+    fi
+}
 
 main() {
     local src=$(pwd) \
@@ -19,7 +30,14 @@ main() {
 
     cross rustc --bin $CRATE_NAME --target $TARGET --release -- -C lto
     
-    cp target/$TARGET/release/$CRATE_NAME $src/$CRATE_NAME-$TARGET
+    if [ $TARGET = x86_64-pc-windows-gnu ]; then
+        suffix=".exe"
+    else
+        suffix=""
+    fi
+    origin=target/$TARGET/release/$CRATE_NAME$suffix
+    try_compress $origin
+    cp $origin $src/$CRATE_NAME-$TARGET$suffix
     # cp target/$TARGET/release/$CRATE_NAME $stage/
 
     # cd $stage
